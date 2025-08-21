e790e146e70a7644659c3146238795838b9dc3d9
Yann Torres <yanngames68@gmail.com>
2025-08-21 20:01:37 -0300

    Add project files.

---
+++ ToolkitConsole/Program.cs
@@ -0,0 +1,36 @@
+﻿var loop = true;
+
+while (loop)
+{
+    ExibirMenu();
+    Console.Write("Digite uma opção: ");
+    var opcaoDigitada = Console.ReadLine();
+
+    switch (opcaoDigitada)
+    {
+        case "1":
+            break;
+        case "0":
+            loop = false;
+            break;
+        default:
+            Console.WriteLine("Número inválido tente novamente");
+            break;
+    }
+}
+
+void ExibirMenu()
+{
+    Console.WriteLine("Projeto Toolkit (versao simples)");
+    Console.WriteLine("1 - Verificar alfabeto e cadeia (Sigma={a,b})");
+    Console.WriteLine("2 - Classificador T/I/N por JSON");
+    Console.WriteLine("3 - Decisor: termina com 'b'?");
+    Console.WriteLine("4 - Avaliador proposicional (P,Q,R)");
+    Console.WriteLine("5 - Reconhecedor: L_par_a e a b*");
+    Console.WriteLine("6 - Problema x instancia por JSON");
+    Console.WriteLine("7 - Decisores: L_fim_b e L_mult3_b");
+    Console.WriteLine("8 - Reconhecedor que pode nao terminar (a^ib ^ i)");
+    Console.WriteLine("9 - Detector ingenuo de loop");
+    Console.WriteLine("10 - Simulador AFD simples (termina com 'b')");
+    Console.WriteLine("0 - Sair");
+}
